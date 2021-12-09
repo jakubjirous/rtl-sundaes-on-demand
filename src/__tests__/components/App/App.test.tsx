a03f3c88c9d15db89356a24083d3052aee523872
Jakub Jirous <info@jakubjirous.cz>
2021-12-08 17:41:05 -0800

    [config] codebase - eslint, prettier

--- src/__tests__/components/App/App.test.tsx
+++ src/__tests__/components/App/App.test.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../../../components/App/App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App/>', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkEl = screen.getByText(/learn react/i);
+    expect(linkEl).toBeInTheDocument();
+  });
+
+  test('renders initial message', () => {
+    render(<App />);
+    const textEl = screen.getByText(/edit and save to reload\./i);
+    expect(textEl).toBeInTheDocument();
+  });
 });
