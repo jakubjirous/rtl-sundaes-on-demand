fbc10e860cdce3cb719f1808c1c565c8808af0e4
Jakub Jirous <info@jakubjirous.cz>
2021-12-10 14:54:38 -0800

    [test] toppings and subtotal

--- ui/src/__tests__/functional/totalUpdates.test.tsx
+++ ui/src/__tests__/functional/totalUpdates.test.tsx
@@ -8,11 +8,11 @@ describe("total updates", () => {
     render(<Options optionType={OptionType.SCOOPS} />);
 
     // make sure total starts out $0.00
-    const scoopSubtotal = screen.getByText("Scoops total: $", {
+    const scoopTotal = screen.getByText("Scoops total: $", {
       exact: false,
     });
 
-    expect(scoopSubtotal).toHaveTextContent("0.00");
+    expect(scoopTotal).toHaveTextContent("0.00");
 
     // update vanilla scoops to 1 and check the subtotal
     const vanillaInputEl = await screen.findByRole("spinbutton", {
@@ -22,7 +22,7 @@ describe("total updates", () => {
     userEvent.clear(vanillaInputEl);
     userEvent.type(vanillaInputEl, "1");
 
-    expect(scoopSubtotal).toHaveTextContent("2.00");
+    expect(scoopTotal).toHaveTextContent("2.00");
 
     // update chocolate scoops to 2 and check the subtotal
     const chocolateInputEl = await screen.findByRole("spinbutton", {
@@ -32,6 +32,35 @@ describe("total updates", () => {
     userEvent.clear(chocolateInputEl);
     userEvent.type(chocolateInputEl, "2");
 
-    expect(scoopSubtotal).toHaveTextContent("6.00");
+    expect(scoopTotal).toHaveTextContent("6.00");
+  });
+
+  test("update toppings subtotal when toppings change", async () => {
+    render(<Options optionType={OptionType.TOPPINGS} />);
+
+    // make sure total starts out $0.00
+    const toppingsTotal = screen.getByText("Toppings total: $", {
+      exact: false,
+    });
+
+    expect(toppingsTotal).toHaveTextContent("0.00");
+
+    // add cherries and check total
+    const cherriesCheckboxEl = await screen.findByRole("checkbox", {
+      name: "Cherries",
+    });
+    userEvent.click(cherriesCheckboxEl);
+    expect(toppingsTotal).toHaveTextContent("1.50");
+
+    // add hot fudge and check total
+    const hotFudgeCheckboxEl = await screen.findByRole("checkbox", {
+      name: "Hot fudge",
+    });
+    userEvent.click(hotFudgeCheckboxEl);
+    expect(toppingsTotal).toHaveTextContent("3.00");
+
+    // add remove fudge and check total
+    userEvent.click(hotFudgeCheckboxEl);
+    expect(toppingsTotal).toHaveTextContent("1.50");
   });
 });
